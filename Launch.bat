864057afa95d11cc1715b952d8600413f1c3bcbd
tgparkk <sttgpark@gmail.com>
2025-04-16 21:08:13 +0900

    characterdata

---
+++ Launch.bat
@@ -0,0 +1,2 @@
+call "D:/Program Files/Epic Games/UE_5.4/Engine/Build/BatchFiles/Build.bat" ^
+-projectfiles -project="%~dp0\D1.uproject" -game -rocket -progress
--- Source/LyraGame/System/LyraAssetManager.h
+++ Source/LyraGame/System/LyraAssetManager.h
@@ -11,6 +11,7 @@ class UPrimaryDataAsset;
 
 class ULyraGameData;
 class ULyraPawnData;
+class UD1CharacterData;
 
 struct FLyraBundles
 {
@@ -50,6 +51,7 @@ public:
 
 	const ULyraGameData& GetGameData();
 	const ULyraPawnData* GetDefaultPawnData() const;
+	const UD1CharacterData& GetCharacterData();
 
 protected:
 	template <typename GameDataClass>
@@ -93,6 +95,9 @@ protected:
 	// Pawn data used when spawning player pawns if there isn't one set on the player state.
 	UPROPERTY(Config)
 	TSoftObjectPtr<ULyraPawnData> DefaultPawnData;
+
+	UPROPERTY(Config)
+	TSoftObjectPtr<UD1CharacterData> CharacterDataPath;
 
 private:
 	// Flushes the StartupJobs array. Processes all startup work.
